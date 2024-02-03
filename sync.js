7f067eae5aaa245d9e32c645b30213d30552ccf7
Abe Haskins <abeisgreat@abeisgreat.com>
2024-02-03 23:59:51 +0000

    Prettier

--- sync.js
+++ sync.js
@@ -1,7 +1,7 @@
 #!/usr/bin/bash node
 import sqlite3 from "sqlite3";
 import yaml from "js-yaml";
-import {readFileSync} from "fs";
+import { readFileSync } from "fs";
 
 const driver = sqlite3.verbose();
 
@@ -15,7 +15,7 @@ const playlistUrl = (playlistId) =>
 const videoUrl = (videoId) =>
   `https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&id=${videoId}&key=${key}`;
 const channelUrl = (channelId) =>
-  `https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&id=${channelId}&key=${key}`
+  `https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&id=${channelId}&key=${key}`;
 const get = async (url) => await (await fetch(url)).json();
 
 const date = (minus) => {
@@ -24,58 +24,61 @@ const date = (minus) => {
   return `${d.getFullYear()}-${d.getMonth()}-${d.getDate()}`;
 };
 
-    let playlistItems = [];
-    for (const playlistId in config.playlists) {
-    const playlist = await get(playlistUrl(config.playlists[playlistId]));
+let playlistItems = [];
+for (const playlistId in config.playlists) {
+  const playlist = await get(playlistUrl(config.playlists[playlistId]));
 
-    playlistItems = [
-        ...playlistItems, 
-        ...playlist.items
-    ]
-    }
+  playlistItems = [...playlistItems, ...playlist.items];
+}
 
-    const videoIds = playlistItems.map((item) => item.contentDetails.videoId);
+const videoIds = playlistItems.map((item) => item.contentDetails.videoId);
 
-    const videoItems = (await get(videoUrl(videoIds))).items;
+const videoItems = (await get(videoUrl(videoIds))).items;
 
-    const videos = videoItems.reduce((obj, v) => {
-        obj[v.id] = {
-            id: v.id,
-            ...v.statistics,
-        };
+const videos = videoItems.reduce((obj, v) => {
+  obj[v.id] = {
+    id: v.id,
+    ...v.statistics,
+  };
 
-        return obj;
-    }, {});
+  return obj;
+}, {});
 
-    const channels = {};
-    for (const channelName in config.channels) {
-        const channelId = config.channels[channelName];
-        channels[channelName] = (await get(channelUrl(channelId))).items[0];
-    }
+const channels = {};
+for (const channelName in config.channels) {
+  const channelId = config.channels[channelName];
+  channels[channelName] = (await get(channelUrl(channelId))).items[0];
+}
 
-    db.serialize(() => {
-        db.run("CREATE TABLE IF NOT EXISTS video_snapshots (id TEXT PRIMARY KEY, date DATE, snapshot TEXT)");
-        db.run("CREATE TABLE IF NOT EXISTS channel_snapshots (id TEXT PRIMARY KEY, date DATE, snapshot TEXT)");
+db.serialize(() => {
+  db.run(
+    "CREATE TABLE IF NOT EXISTS video_snapshots (id TEXT PRIMARY KEY, date DATE, snapshot TEXT)",
+  );
+  db.run(
+    "CREATE TABLE IF NOT EXISTS channel_snapshots (id TEXT PRIMARY KEY, date DATE, snapshot TEXT)",
+  );
 
-        for (const channelName in config.channels) {
-            const channelRaw = channels[channelName];
-            const channel = {
-            id: channelRaw.id,
-            ...channelRaw.statistics
-            }
+  for (const channelName in config.channels) {
+    const channelRaw = channels[channelName];
+    const channel = {
+      id: channelRaw.id,
+      ...channelRaw.statistics,
+    };
 
-            db.run("INSERT INTO channel_snapshots(id, date, snapshot) VALUES(?, ?, ?) ON CONFLICT(ID) DO UPDATE SET snapshot = excluded.snapshot", 
-                [`${date()}-${channel.id}`, 
-                new Date(), 
-                JSON.stringify(channel)]);
+    db.run(
+      "INSERT INTO channel_snapshots(id, date, snapshot) VALUES(?, ?, ?) ON CONFLICT(ID) DO UPDATE SET snapshot = excluded.snapshot",
+      [`${date()}-${channel.id}`, new Date(), JSON.stringify(channel)],
+    );
+  }
 
-        }
+  for (const record in videos) {
+    const obj = videos[record];
+    db.run(
+      "INSERT INTO video_snapshots(id, date, snapshot) VALUES(?, ?,?) ON CONFLICT(id) DO UPDATE SET snapshot = excluded.snapshot",
+      [`${date()}-${obj.id}`, new Date(), JSON.stringify(obj)],
+    );
+  }
+});
+db.close();
 
-    for (const record in videos) {
-        const obj = videos[record];
-        db.run("INSERT INTO video_snapshots(id, date, snapshot) VALUES(?, ?,?) ON CONFLICT(id) DO UPDATE SET snapshot = excluded.snapshot", [`${date()}-${obj.id}`, new Date(), JSON.stringify(obj)]);
-    }
-    });
-    db.close();
-
-    console.log("Daily database data added.");
+console.log("Daily database data added.");
